8b58fb4acba70d9544ed7f977877039706922069
Vladislav812 <bolotin_vlad@list.ru>
2022-07-07 20:05:26 +0300

    realize thread-safe counter via AtomicInteger

--- src/main/java/xyz/kolokolov/simpleservice/controller/HelloController.java
+++ src/main/java/xyz/kolokolov/simpleservice/controller/HelloController.java
@@ -5,17 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
-    int counter;
+    AtomicInteger counter = new AtomicInteger(0);
     @GetMapping
     public ResponseEntity<String> hello(){
-        counter++;
-        return ResponseEntity.ok("Hello " + counter + " times");
-
+        counter.getAndIncrement();
+        return ResponseEntity.ok("Hello " + counter.get() + " times");
     }
-
-
-
 }
